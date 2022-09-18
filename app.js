943f378f6805920b012c559131b650d2d9dfcef8
calupama <calupama2@gmail.com>
2022-09-18 09:22:19 -0400

    el primer commit del proyecto

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/home.html'));
+   
+});
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/src/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/src/views/login.html');
+});
+
+app.post('/login', (req,res)=>{
+    res.redirect('/');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/src/views/register.html');
+});
+app.post('/register', (req,res)=>{  
+    res.redirect('/');
+});
