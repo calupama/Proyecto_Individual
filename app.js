7894e58df5001fdaea00fce01562d373315c9c6f
calupama <calupama2@gmail.com>
2022-09-20 21:16:04 -0400

    nuevos cambios en app.js y procfile

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
    
 });
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
 
 app.get('/', (req,res)=>{
